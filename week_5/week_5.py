5ca0a08820fb492768f70100a03b9092d76ea340
arielkay9 <122850391+arielkay9@users.noreply.github.com>
2023-05-05 17:56:21 -0400

    Update week_5.py

--- week_5/week_5.py
+++ week_5/week_5.py
@@ -1,1 +1,22 @@
+import os
+import json
 
+with open ('artworks.json', 'r') as f:
+    data = json.load(f)
+
+nationalities = {}
+for art_piece in data:
+    nationality = art_piece['Nationality']
+    if isinstance(nationality, list):
+        nationality = ', '.join(nationality)
+    if nationality not in nationalities:
+        nationalities[nationality] = []
+    nationalities[nationality].append(art_piece)
+
+if not os.path.exists('res'):
+    os.makedirs('res')
+
+for nationality, art_piece in nationalities.items():
+    filename = f'res/{nationality}.json'
+    with open(filename, 'w') as f:
+        json.dump(art_piece, f, indent=2)
