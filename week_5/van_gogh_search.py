fc90127a55102e4502e4a54e2b258f64835dab11
arielkay9 <122850391+arielkay9@users.noreply.github.com>
2023-05-05 18:09:26 -0400

    Update van_gogh_search.py

--- week_5/van_gogh_search.py
+++ week_5/van_gogh_search.py
@@ -1,1 +1,20 @@
+import requests
+import pprint
 
+url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
+
+params = {'q' : 'van gogh',
+          'isOnView' : 'true',
+          'hasImages' : 'true',
+          'ObjectIDs' : ''
+
+}
+
+response = requests.get(url, params=params)
+
+if response.status_code == 200:
+    object_ids = response.json()['objectIDs']
+    
+    pretty = pprint.PrettyPrinter(indent=2)
+
+    pretty.pprint(object_ids)
