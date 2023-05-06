3304c4e77e5ae927a38ff3d9363a006bbf6644db
arielkay9 <122850391+arielkay9@users.noreply.github.com>
2023-05-05 20:32:08 -0400

    Update ex1.py

--- week_8/ex1.py
+++ week_8/ex1.py
@@ -1,1 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
 
+url = 'https://www.moma.org/artists/?exhibition_id=5224&page=1'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.content, 'html.parser')
+
+h3_element = soup.find('h3', class_='typography')
+
+if h3_element is not None:
+    artist_names = h3_element.get_text(strip=True)
+    print(artist_names)
