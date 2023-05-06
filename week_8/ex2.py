591f0ad9d7d290c4a5d194ffaa5037c5fa6befae
arielkay9 <122850391+arielkay9@users.noreply.github.com>
2023-05-05 21:14:04 -0400

    Update ex2.py

--- week_8/ex2.py
+++ week_8/ex2.py
@@ -6,23 +6,20 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.content, 'html.parser')
 
-artist_list = soup.find_all('li', class_='grid-item')
+artist_list = soup.find_all('h3', class_='typography')
+
+artists_dict = {}
 
 for artist in artist_list:
-    # Extract artist name
-    artist_name = artist.find('h3', class_='typography').text.strip()
-    
-    # Extract artist bio
-    artist_bio = artist.find('p', class_='typography--small').text.strip() if artist.find('p', class_='typography--small') else 'No bio available'
-    
-    # Extract artist works
-    artist_works_list = artist.find_all('p', class_='text--caption')
-    if artist_works_list:
-        artist_works = [work.text.strip() for work in artist_works_list]
-    else:
-        artist_works = ['No works available']
-    
-    # Print artist information
-    print('Artist Name:', artist_name)
-    print('Artist Bio:', artist_bio)
-    print('Artist Works:', artist_works)
+    artist_name = artist.text.strip()
+    bio = ""
+    works = "No works available"
+    bio_elem = artist.find_next_sibling('p', class_='typography')
+    if bio_elem:
+        bio = bio_elem.text.strip()
+    works_elem = bio_elem.find_next_sibling('p', class_='typography')
+    if works_elem and 'works online' in works_elem.text:
+        works = works_elem.text.strip()
+    artists_dict[artist_name] = {"bio": bio, "works": works}
+
+print(artists_dict)
